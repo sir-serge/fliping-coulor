d46eee00e6d9a08f1a28c9cb9ef82eae4c8c2142
sir-serge <irkzsrg@gmail.com>
2024-05-26 13:35:29 +0200

    hex shuffle

--- hex.js
+++ hex.js
@@ -2,6 +2,15 @@ const colorsNumbers=[0,1,2,3,4,5,6,7,8,9];
 const colorsLetters=["a","b","c","e","f","g"];
 const clicking=document.getElementById("clicking");
 const fliping=document.querySelector(".fliping")
+clicking.addEventListener("click", function(){
+    let recordingColors="#";
+    for (let i=0;i<3;i++){
+        recordingColors+=getRandomNumber();
+    }
+    // console.log("#",recordingColors[]);
+    document.body.style.backgroundColor=recordingColors;
+    fliping.textContent=fliping[String(recordingColors)];
+});
 function getRandomNumber(){
     let n=Math.floor(Math.random()*colorsNumbers.length);
     let y=Math.floor(Math.random()*colorsLetters.length);
